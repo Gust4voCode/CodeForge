6a0de467bb15f4137ccfe88e751c2ccb0afe89c2
Gustavo Coelho <gustavocorvelho@gmail.com>
2024-11-13 23:09:11 -0300

    more problems solved

---
+++ beecrowd/Problems/Beginner/1006.py
@@ -0,0 +1,9 @@
+# Average 2
+
+A= float(input())
+B = float(input())
+C = float(input())
+
+if A <= 10 and B <= 10 and C <= 10:
+    Media=((A * 2)+(B * 3) + (C * 5))/10
+print('MEDIA = {:.1f}'.format(Media))
---
+++ beecrowd/Problems/Beginner/1007.py
@@ -0,0 +1,10 @@
+# Difference
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+product = (A * B - C * D)
+
+print(f'DIFERENCA = {product}')
