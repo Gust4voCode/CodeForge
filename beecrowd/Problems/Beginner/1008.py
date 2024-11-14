cff83ae44863e04c954dffe3e915cc4d6244941c
Gustavo Coelho <gustavocorvelho@gmail.com>
2024-11-14 01:48:44 -0300

    1008

---
+++ beecrowd/Problems/Beginner/1008.py
@@ -0,0 +1,10 @@
+# Salary
+
+number = int(input())
+hours = int(input())
+amount = float(input())
+
+salary = hours * amount
+
+print(f'NUMBER = {number}')
+print('SALARY = U$ {:.2f}'.format(salary))
