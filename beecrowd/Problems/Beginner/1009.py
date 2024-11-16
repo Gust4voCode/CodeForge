4eb3bd38232e258ae6905ebf376b6f26e0e458b9
Gustavo Coelho <gustavocorvelho@gmail.com>
2024-11-15 23:34:21 -0300

    1009

---
+++ beecrowd/Problems/Beginner/1009.py
@@ -0,0 +1,11 @@
+# Salary with Bonus
+
+name = input()
+
+salary = float(input())
+
+total_sold = float(input())
+
+total = ( total_sold * 0.15 ) + salary
+
+print('TOTAL = R$ {:.2f}'.format(total))
